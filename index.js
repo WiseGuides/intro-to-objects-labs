d38ebbb2240c6e02f7ab34e9618d83d11cb16f44
Hal Helms <hal.helms@gmail.com>
2019-01-22 08:58:36 -0800

    9 exercises

---
+++ index.js
@@ -0,0 +1,38 @@
+// Intro to Objects Lab
+// Rev. 22Jan2019
+
+/*
+1. Create an object, me, that has your personal information including first name, last name, birthday. Output the object.
+*/
+
+/*
+2. Create three objects with data from friends or family -- or just made up. Now create an array, them, and put them objects into the array.
+*/
+
+/*
+3. Create a new object, address, with your street, city, state, and zip. (Feel free to make these up too). Attach this object to your 'me' object, giving it a key of 'address'.
+*/
+
+/*
+4. Attach 'them' to 'me', using a key of 'friends'.
+*/
+
+/*
+5. Output the second persons in 'friends' (starting from the object, me).
+*/
+
+/*
+6. Use square bracket notation to output your first name and last name.
+*/
+
+/*
+7. Create a method for 'me', 'introduceFriends', that loops over each friend, outputting their first and last names.
+*/
+
+/*
+8. Create a variable, 'myClone', that points to 'me'. Change the address for 'myClone'. What happened to the address of 'me'? Why did that happen?
+*/
+
+/*
+9. Create a method, 'render', in 'me.address', that outputs the address.
+*/
